0ba5c1593b820eb8129f6d9b17b8cd3b1301ba5d
Szuum <szymonurb@student.agh.edu.pl>
2022-10-09 21:04:40 +0200

    Lab1 dokończenie

--- oolab/oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/oolab/src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,30 @@
 package agh.ics.oop;
 
 public class World {
-
+    public static void main(String[] args) {
+        System.out.println("System wystartował");
+        Direction t[] = {Direction.BACKWARD, Direction.FORWARD, Direction.BACKWARD, Direction.LEFT, Direction.RIGHT};
+        run(t);
+        System.out.println("System zakończył działanie");
+    }
+    static void run(Direction tab[]) {
+        int n=tab.length;
+        for (int i=0; i<n; i++) {
+            Direction argument = tab[i];
+            switch (argument) {
+                case FORWARD:
+                    System.out.println("Zwierzak idzie do przodu");
+                        break;
+                case BACKWARD:
+                    System.out.println("Zwierzak idzie do tyłu");
+                        break;
+                case RIGHT:
+                    System.out.println("Zwierzak skręca w prawo");
+                        break;
+                case LEFT:
+                    System.out.println("Zwierzak skręca w lewo");
+                        break;
+            }
+        }
+    }
 }
